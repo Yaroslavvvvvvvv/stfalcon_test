51cb1431b04a1ec971e20ea59958bdd80323e47e
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-26 21:21:51 +0300

    end header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -4,7 +4,9 @@
       <nav class="navbar navbar-expand-lg ">
         <div class="container-fluid">
           <a class="navbar-brand my_brand" href="#">LOGO</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                  aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse my_menu" id="navbarSupportedContent">
@@ -29,33 +31,35 @@
               <li class="nav-item">
                 <a class="nav-link my_nav" href="#">Support</a>
               </li>
-                <li class="dropdown">
-                  <button class="btn header_btn dropdown-toggle" type="button" id="dropdownMenuButton1"
-                          data-bs-toggle="dropdown" aria-expanded="false">
-                    Download
-                  </button>
-                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item btn_item" href="#">App Store</a></li>
-                    <li><a class="dropdown-item btn_item" href="#">Google Play</a></li>
-                  </ul>
-                </li>
-                <li class="dropdown">
-                  <a class="nav-link my_nav d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown"
-                     aria-expanded="false">
-                    Eng
-                    <img src="../assets/img/header/arrow-down.svg" alt="logo" class="mw-100">
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li class="d-flex align-items-center">
-                      <img src="../assets/img/header/sweden.svg" alt="logo" class="mw-100 m-2">
-                      <a class="dropdown-item btn_item p-lg-0 p-md-1 p-sm-1" href="#">Sverige</a>
-                    </li>
-                    <li class="d-flex align-items-center">
-                      <img src="../assets/img/header/united-kingdom.svg" alt="logo" class="mw-100 m-2">
-                      <a class="dropdown-item btn_item p-lg-0 p-md-1 p-sm-1" href="#">English</a>
-                    </li>
-                  </ul>
-                </li>
+              <li class="dropdown">
+                <button class="btn header_btn dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                  Download
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li><a class="dropdown-item btn_item" href="#">App Store</a></li>
+                  <li><a class="dropdown-item btn_item" href="#">Google Play</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a class="nav-link my_nav d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown"
+                   aria-expanded="false">
+                  {{ currentLanguage === 'en' ? 'Eng' : 'Sve' }}
+                  <img src="../assets/img/header/arrow-down.svg" alt="logo" class="mw-100">
+                </a>
+                <ul class="dropdown-menu">
+                  <li class="d-flex align-items-center">
+                    <img src="../assets/img/header/sweden.svg" alt="logo" class="mw-100 m-2">
+                    <a class="dropdown-item btn_item p-lg-0 p-md-1 p-sm-1" href="#" @click="changeLanguage('sv')">
+                      Sverige</a>
+                  </li>
+                  <li class="d-flex align-items-center">
+                    <img src="../assets/img/header/united-kingdom.svg" alt="logo" class="mw-100 m-2">
+                    <a class="dropdown-item btn_item p-lg-0 p-md-1 p-sm-1" href="#" @click="changeLanguage('en')">
+                      English</a>
+                  </li>
+                </ul>
+              </li>
             </ul>
           </div>
         </div>
@@ -66,8 +70,20 @@
 
 <script>
 export default {
-  name: "MyHeader"
+  name: "MyHeader",
+  data() {
+    return {
+      currentLanguage: 'en'
+    };
+  },
+  methods: {
+    changeLanguage(lang) {
+      this.currentLanguage = lang;
+    }
+  }
 }
+
+
 </script>
 
 <style scoped lang="scss">
