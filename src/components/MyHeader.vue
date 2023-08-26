3b1f67bcea70715745242938f759beb3248120b3
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-26 17:23:02 +0300

    do header

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -7,8 +7,8 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 mb-lg-0 m-auto">
+          <div class="collapse navbar-collapse my_menu" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link my_nav" aria-current="page" href="#">Home</a>
               </li>
@@ -29,30 +29,29 @@
               <li class="nav-item">
                 <a class="nav-link my_nav" href="#">Support</a>
               </li>
+                <li class="dropdown">
+                  <button class="btn header_btn dropdown-toggle" type="button" id="dropdownMenuButton1"
+                          data-bs-toggle="dropdown" aria-expanded="false">
+                    Download
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item btn_item" href="#">App Store</a></li>
+                    <li><a class="dropdown-item" href="#">Google Play</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a class="nav-link my_nav d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown"
+                     aria-expanded="false">
+                    Eng
+                    <img src="../assets/img/header/arrow-down.svg" alt="logo" class="mw-100">
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
             </ul>
-            <form class="d-flex">
-              <div class="dropdown">
-                <button class="btn header_btn dropdown-toggle" type="button" id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                  Download
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                  <li><a class="dropdown-item btn_item" href="#">App Store</a></li>
-                  <li><a class="dropdown-item" href="#">Google Play</a></li>
-                </ul>
-              </div>
-              <div class="dropdown">
-                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown link
-                </a>
-
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </div>
-            </form>
           </div>
         </div>
       </nav>
