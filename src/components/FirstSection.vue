a3aee5ecd84cf3db296abc89e5c21b2eb18e19ac
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-27 23:33:56 +0300

    end first section

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -1,21 +1,50 @@
 <template>
   <section class="first_section">
     <div class="container">
-      <div class="purple_block position-relative mw-100">
-        <div class="position-absolute">
-          <img src="../assets/img/first_section/first_line.svg" alt="line" class="mw-100 first_line">
+      <div class="position-relative">
+        <div class="purple_block mw-100">
+          <div class="position-absolute">
+            <img src="../assets/img/first_section/first_line.svg" alt="line" class="mw-100 first_line">
+          </div>
+          <div class="position-absolute">
+            <img src="../assets/img/first_section/second_line.svg" alt="line" class="mw-100 second_line">
+          </div>
+          <div class="position-absolute">
+            <img src="../assets/img/first_section/third_line.svg" alt="line" class="mw-100 third_line">
+          </div>
+          <div class="position-absolute">
+            <img src="../assets/img/first_section/fourth_line.svg" alt="line" class="mw-100 fourth_line">
+          </div>
+          <div class="position-absolute">
+            <img src="../assets/img/first_section/fifth_line.svg" alt="line" class="mw-100 fifth_line">
+          </div>
         </div>
         <div class="position-absolute">
-          <img src="../assets/img/first_section/second_line.svg" alt="line" class="mw-100 second_line">
+          <img src="../assets/img/first_section/iphone.png" alt="iphone" class="mw-100 iphone">
         </div>
         <div class="position-absolute">
-          <img src="../assets/img/first_section/third_line.svg" alt="line" class="mw-100 third_line">
+          <h1>Strong for life<br>with us</h1>
+          <hr class="colored_line">
+          <p>The best Pre/postnatal fitness app! 100<br>
+            exercises divided in before, during and<br>
+            after pregnancy. Stay strong and healthy<br>
+            through all stages of pregnancy. </p>
         </div>
         <div class="position-absolute">
-          <img src="../assets/img/first_section/fourth_line.svg" alt="line" class="mw-100 fourth_line">
+          <a href="#">
+            <img src="../assets/img/first_section/app_store.svg" alt="logo" class="mw-100 app_store">
+          </a>
         </div>
         <div class="position-absolute">
-          <img src="../assets/img/first_section/fifth_line.svg" alt="line" class="mw-100 fifth_line">
+          <a href="#">
+            <img src="../assets/img/first_section/google_play.svg" alt="logo" class="mw-100 google_play">
+          </a>
+        </div>
+        <div class="position-absolute">
+          <img src="../assets/img/first_section/left_phone.png" alt="image" class="mw-100 left_phone">
+        </div>
+        <div class="position-absolute">
+          <img src="../assets/img/first_section/right_phone.png" alt="image" class="mw-100 right_phone">
         </div>
       </div>
     </div>
