ae68a97a3f74e3b0bd8f3e6a9ab7b764a7b55b7a
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-29 02:30:23 +0300

    do third section

--- src/components/ThirdSection.vue
+++ src/components/ThirdSection.vue
@@ -1,12 +1,36 @@
 <template>
   <section class="third_section">
     <div class="container">
-      <div class="text-center">
+      <div class="text-center first_third">
         <h2>Before, during and after pregnancy</h2>
         <p class="header_text">The app is divided into three different stages to help you choose<br>
           appropriate exercises. Each exercise is carefully selected to ensure safe and<br>
           appropriate adjustments to follow the pregnancy’s progress.</p>
         <hr class="colored_line">
+      </div>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 mt-5">
+          <h3>Before pregnancy</h3>
+          <hr class="next_line">
+          <p class="text_regular">
+            Start preparing for pregnancy with safe exercises that build<br>
+            up a strong body.</p>
+          <h5>Trimesters</h5>
+          <p class="text_regular">The pregnancy is divided into first trimester, second trimester<br>
+            part 1 and 2, and third trimester part 1 and 2 to provide<br>
+            exercises that are safe for each stage of the pregnancy. Each<br>
+            exercise comes with a video and a description so you can<br>
+            safely perform each exercise. Simple put in your due date and<br>
+            you will be taken to the proper stage.</p>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 right_block">
+          <div class="position-relative">
+            <img src="../assets/img/third_section/right.svg" alt="block" class="mw-100">
+            <div class="position-absolute">
+              <img src="../assets/img/third_section/right_iphone.png" alt="iphone" class="right_iphone">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
