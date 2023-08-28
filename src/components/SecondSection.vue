867e159df52f7811b451d6e062a14a2b0712df52
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-08-28 23:56:21 +0300

    start second section

---
+++ src/components/SecondSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="second_section">
+    <div class="container">
+      <div class="text-center">
+        <h2>How does it work?</h2>
+        <h3>Easy, fun & essential training for moms</h3>
+        <hr class="colored_line">
+      </div>
+      <div class="row mt-5">
+        <div class="col-lg-6 col-md-6 col-md-6">
+          <div class="d-flex align-items-start">
+            <img src="../assets/img/second_section/exercises.svg" alt="logo" class="mw-100">
+            <div>
+              <h5>Browse exercises</h5>
+              <p>Browse over 100 exercises with a video or picture and a<br>
+                description.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-md-6">
+          <div class="d-flex align-items-start">
+            <img src="../assets/img/second_section/timeline.svg" alt="logo" class="mw-100">
+            <div>
+              <h5>Create timeline</h5>
+              <p>Take pictures along the way during and after the<br>
+                pregnancy to see the transformation.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SecondSection"
+}
+</script>
+
+<style scoped lang="scss">
+@import "./src/assets/styles/second_section";
+
+</style>
--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -1,13 +1,15 @@
 <template>
-<FirstSection/>
+  <FirstSection/>
+  <SecondSection/>
 </template>
 
 <script>
 import FirstSection from "@/components/FirstSection.vue";
+import SecondSection from "@/components/SecondSection.vue";
 
 export default {
   name: "MyHome",
-  components: {FirstSection}
+  components: {SecondSection, FirstSection}
 }
 </script>
 
